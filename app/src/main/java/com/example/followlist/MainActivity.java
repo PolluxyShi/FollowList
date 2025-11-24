7cea21c5776e64a92ed13e4f90dfe0541f30417d
Polluxy <1279870296@qq.com>
2025-11-24 13:51:50 +0800

    justify project structure

--- app/src/main/java/com/example/followlist/MainActivity.java
+++ app/src/main/java/com/example/followlist/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.followlist.viewpager.ViewPagerAdapter;
+import com.example.followlist.ui.viewpager.ViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
