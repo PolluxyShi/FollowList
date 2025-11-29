126993fd1ed194095ad634b81d493cbd024039c6
Polluxy <1279870296@qq.com>
2025-11-29 18:21:05 +0800

    fix memory leak on BlankFragment

--- app/src/main/java/com/example/followlist/MainActivity.java
+++ app/src/main/java/com/example/followlist/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.followlist;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -13,6 +12,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager;
     private TabLayout tabLayout;
+    private TabLayoutMediator tabLayoutMediator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +30,24 @@ public class MainActivity extends AppCompatActivity {
         // 设置默认页面为第2个页面（索引为1）
         viewPager.setCurrentItem(1, false);
 
-        // 将 TabLayout 与 ViewPager2 关联
-        new TabLayoutMediator(tabLayout, viewPager,
-                new TabLayoutMediator.TabConfigurationStrategy() {
-                    @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                        // 为不同位置设置不同的标签
-                        switch (position) {
-                            case 0:
-                                tab.setText("互关");
-                                break;
-                            case 1:
-                                tab.setText("关注");
-                                break;
-                            case 2:
-                                tab.setText("粉丝");
-                                break;
-                            case 3:
-                                tab.setText("朋友");
-                                break;
-                        }
+        tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("互关");
+                            break;
+                        case 1:
+                            tab.setText("关注");
+                            break;
+                        case 2:
+                            tab.setText("粉丝");
+                            break;
+                        case 3:
+                            tab.setText("朋友");
+                            break;
                     }
                 }
-        ).attach();
+        );
+        tabLayoutMediator.attach();
     }
 }
